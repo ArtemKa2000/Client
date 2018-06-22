6acddf0a0f767f57074325e6306dc20d3c594169
Artem Kamantsev <art.tem.ka2000@gmail.com>
2018-06-22 19:56:14 +0300

    Text sending. Code template.

--- app/src/main/java/com/example/logorecognizer/MainActivity.java
+++ app/src/main/java/com/example/logorecognizer/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.logorecognizer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.*;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnSend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSend=(Button)findViewById(R.id.btnSend);
+        OnClickListener listener=new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //sending text to server
+            }
+        };
+        btnSend.setOnClickListener(listener);
     }
 }
